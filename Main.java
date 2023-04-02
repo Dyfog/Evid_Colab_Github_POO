d3f40615cb3e60efb85dc9f9814f9657884e0b5f
Ztellar <b.salamanca01@ufromail.cl>
2023-04-02 19:32:36 -0300

    1.1

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        
+
     }
     public static int multiply(int a, int b){
+        System.out.println("Método Multiplicar creado por Benjamín Salamanca");
         return a*b;
     }
 }
