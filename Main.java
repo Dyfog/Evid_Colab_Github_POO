a14bc36ef4356b1c784ccaa38cc9137eea550899
Zopa2222 <s.medina08@ufromail.cl>
2023-04-02 17:55:34 -0400

    1.1
    Añadido un output del nombre del método y su creador

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
     }
     public static int sumar(int x, int y){
+        System.out.println("Usando método sumar, creado por Sofia Medina");
         int sum = x+y;
         return sum;
     }
