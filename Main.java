4f67765de07e846f15753100653166cf05e8919c
Dyfog <111368031+Dyfog@users.noreply.github.com>
2023-04-02 18:47:29 -0400

    inplementacion main del archivo
    implementacion del codigo que usa los metodos creados en cada branch

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("ingrese 2 numeros");
+        int num1 = in.nextInt();
+        int num2 = in.nextInt();
+        System.out.println("la suma de ambos numeros es: "+sumar(num1,num2));
+        System.out.println("la resta de ambos numeros es: "+restar(num1,num2));
+        System.out.println("la multiplicacion de ambos numeros es: "+multiply(num1,num2));
+
     }
     public static int sumar(int x, int y){
         System.out.println("Usando método sumar, creado por Sofia Medina");
